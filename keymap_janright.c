07cccfb870c66963637e07017fe32d4f55397dc8
Jan Londén <jjlonden@gmail.com>
2015-10-16 14:26:53 +0200

    arst

--- keymap_janright.c
+++ keymap_janright.c
@@ -7,14 +7,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = {
     {KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS, KC_TRNS, CM_J, CM_L, CM_U, CM_Y, CM_SCLN, M(0)},
     {KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS, KC_TRNS, CM_H, CM_N, CM_E, CM_I, CM_O, KC_QUOT},
-    {KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS, KC_TRNS, CM_K, CM_M, CM_COMM, CM_DOT, CM_SLSH, KC_RALT}, 
+    {KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS, KC_TRNS, CM_K, CM_M, CM_COMM, CM_DOT, CM_SLSH, KC_RALT},
     {FUNC(30), CM_DOT, KC_ENT, KC_TRNS, KC_TRNS, FUNC(20), KC_RSFT, FUNC(10), KC_ENT, KC_RSFT, KC_RCTL, FUNC(31)}
   },
 
   // momentary layers
 
   [10] = {
-    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL, FUNC(11), RALT(KC_P), KC_TRNS},
+    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL, FUNC(11), KC_TRNS, KC_TRNS},
     {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_6, KC_7, KC_8, KC_9, KC_0, KC_TRNS},
     {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), KC_TRNS},
     {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, FUNC(10), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
@@ -29,8 +29,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [20] = {
     {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, KC_TRNS, S(KC_LBRC), S(KC_RBRC), S(KC_BSLS), KC_TRNS, KC_TRNS},
-    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F2, KC_F11, KC_F12, FUNC(21), KC_TRNS},
-    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS},
+    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RALT(CM_Q), RALT(CM_W), RALT(KC_P), FUNC(21), KC_TRNS},
+    {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F2, KC_F11, KC_F12, KC_TRNS, KC_TRNS},
     {KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, FUNC(20), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS}
   },
 
@@ -70,12 +70,12 @@ const uint16_t PROGMEM fn_actions[] = {
 
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) 
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   switch(id) {
     case 0:
       return MACRODOWN(TYPE(CM_A), TYPE(CM_R), TYPE(CM_S), TYPE(CM_T), TYPE(KC_ENT), END);
     break;
-  } 
+  }
   return MACRO_NONE;
 };
