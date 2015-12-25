ecdab6fe5839bbf970eb0fca37f15526fda8f7c1
Jan Londén <jjlonden@gmail.com>
2015-12-25 16:32:40 +0100

    arst

--- keymap_janright.c
+++ keymap_janright.c
@@ -5,10 +5,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // default layers
 
   [0] = {
-    {KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS, CM_J, CM_L, CM_U, CM_Y, CM_SCLN, M(0)},
+    {KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS, KC_TRNS, CM_J, CM_L, CM_U, CM_Y, CM_SCLN, KC_TRNS},
     {KC_TRNS, KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS, CM_H, CM_N, CM_E, CM_I, CM_O, KC_QUOT},
     {KC_TRNS, KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS, CM_K, CM_M, CM_COMM, CM_DOT, CM_SLSH, KC_RALT},
-    {KC_TRNS, FUNC(30), CM_DOT, KC_ENT, KC_TRNS, FUNC(20), KC_RSFT, FUNC(10), KC_ENT, KC_RSFT, KC_RCTL, FUNC(31)}
+    {M(0), FUNC(30), CM_DOT, KC_ENT, KC_TRNS, FUNC(20), KC_RSFT, FUNC(10), KC_ENT, KC_RSFT, KC_RCTL, FUNC(31)}
   },
 
   // momentary layers
